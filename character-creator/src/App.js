128f20c491599f17706194d238bfdd9b1280acfc
Taylor Phillips <taylorscottphillips@gmail.com>
2022-01-08 14:52:33 -0600

    wip: Module 2

---
+++ character-creator/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import CharacterEditor from './components/CharacterEditor';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <CharacterEditor />
+      <div class="grey"></div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
